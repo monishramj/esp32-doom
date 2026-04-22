ac572020eca34d55f7344f3d3062e6ce1eaecc44
monishramj <monishramj621@gmail.com>
2026-04-22 04:01:20 -0400

    resolve dram overflow and linker memory errors

--- main/main.c
+++ main/main.c
@@ -2,20 +2,24 @@
 #include <stdio.h>
 #include "doomgeneric.h"
 
-void DG_Init() { }
+// global vars for network/logic
+int drone = 0;
+int net_client_connected = 0;
 
-void DG_DrawFrame() { }
+// window title (js print it for now)
+void DG_SetWindowTitle(const char * title) {
+    printf("doom window: %s\n", title);
+}
 
-uint32_t DG_GetTicksMs() { return 0; }
-
-void DG_SleepMs(uint32_t ms) { }
-
-int DG_GetKey(int* pressed, unsigned char* key) { return 0; }
+void DG_Init() {}
+void DG_DrawFrame() {}
+uint32_t DG_GetTicksMs() {return 0;}
+void DG_SleepMs(uint32_t ms) {}
+int DG_GetKey(int* pressed, unsigned char* key) {return 0;}
 
 void app_main(void) {
+
     doomgeneric_Create(0, NULL);
-    
-    while(1) {
-        doomgeneric_Tick();
-    }
+    while (1) {doomgeneric_Tick();}
+
 }
